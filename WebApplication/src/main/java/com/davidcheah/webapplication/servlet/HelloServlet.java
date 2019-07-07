5ac749c6e7036be48b70f01a4ca3f676d3fa518f
DavidCheah <tattwei46@gmail.com>
2019-07-07 17:03:44 +0800

    Initial commit

---
+++ WebApplication/src/main/java/com/davidcheah/webapplication/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.davidcheah.webapplication.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>Hello World</h1>");
+		out.println("<p>The time is " + new Date() + "</p>");
+		out.println("</body></html>");
+		out.close();
+	}
+}
